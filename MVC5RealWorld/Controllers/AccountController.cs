9ab0ef32a02ecae77c8587d499cdb68190c87338
sateeshh221b <85143452+sateeshh221b@users.noreply.github.com>
2021-11-16 21:37:13 +0530

    MVC5LogglyDemo

---
+++ MVC5RealWorld/Controllers/AccountController.cs
@@ -0,0 +1,98 @@
+﻿using System.Web.Mvc;
+using System.Web.Security;
+using MVC5RealWorld.Models.ViewModel;
+using MVC5RealWorld.Models.EntityManager;
+using System;
+using log4net;
+
+namespace MVC5RealWorld.Controllers
+{
+    public class AccountController : Controller
+    {
+        readonly ILog logger;
+
+        public AccountController()
+        {
+            logger = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        }
+
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SignUp(UserSignUpView USV)
+        {
+            if (ModelState.IsValid)
+            {
+                //Log.Log.Info($"{USV.LoginName} wants to signup, wants to have role of {USV.RoleName}");
+                logger.Info($"{DateTime.Now.ToString("MMddyyyyhhmmss")} - {USV.LoginName} wants to signup, wants to have role of {USV.RoleName}");
+
+                UserManager UM = new UserManager();
+                if (!UM.IsLoginNameExist(USV.LoginName))
+                {
+                    UM.AddUserAccount(USV);
+                    FormsAuthentication.SetAuthCookie(USV.FirstName, false);
+                    logger.Info($"{DateTime.Now.ToString("MMddyyyyhhmmss")} - SignUp success for {USV.LoginName}");
+                    return RedirectToAction("Welcome", "Home");
+                }
+                else
+                {
+                    logger.Error($"{DateTime.Now.ToString("MMddyyyyhhmmss")} - SignUp failed for {USV.LoginName}, login name already taken");
+                    ModelState.AddModelError("", "Login Name already taken.");
+                }
+            }
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult SignOut()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Index", "Home");
+        }
+
+
+        public ActionResult LogIn()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult LogIn(UserLoginView ULV, string returnUrl)
+        {
+            if (ModelState.IsValid)
+            {
+                //Log.Log.Info($"{ULV.LoginName} initiated a login");
+
+                UserManager UM = new UserManager();
+                string password = UM.GetUserPassword(ULV.LoginName);
+
+                if (string.IsNullOrEmpty(password))
+                {
+                    //Log.Log.Error($"{DateTime.Now.ToString("MMddyyyyhhmmss")} - {ULV.LoginName} login or password provided is incorrect ");
+                    ModelState.AddModelError("", "The user login or password provided is incorrect.");
+                }
+                else
+                {
+                    if (ULV.Password.Equals(password))
+                    {
+                        //Log.Log.Info($"{DateTime.Now.ToString("MMddyyyyhhmmss")} - {ULV.LoginName} login successfull");
+                        //FormsAuthentication.SetAuthCookie(ULV.LoginName, false);
+                        //return RedirectToAction("Welcome", "Home");
+                        FormsAuthentication.RedirectFromLoginPage(ULV.LoginName, false);
+                    }
+                    else
+                    {
+                        //Log.Log.Error($"{DateTime.Now.ToString("MMddyyyyhhmmss")} - {ULV.LoginName} - The password provided is incorrect");
+                        ModelState.AddModelError("", "The password provided is incorrect.");
+                    }
+                }
+            }
+
+            //Log.Log.Error($"{ULV.LoginName} login failed at {DateTime.Now.ToString("MMddyyyyhhmmss")} got redirected to login page");
+            return View(ULV);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,23 @@
+# ASPNETMVC5F with log4net-loggly
+log4net library is used to push logs to loggly
+
+1. Build and restore Nu-get packages.
+2. Create a free account on https://my.solarwinds.cloud/login
+3. Get the Customer Token from your loggly account and update the log4net section in Web.config
+
+References:
+* Log4Net and .NET Logs - https://documentation.solarwinds.com/en/success_center/loggly/content/admin/net-logs.htm?cshid=loggly_net-logs
+
+* Customer Token - https://documentation.solarwinds.com/en/success_center/loggly/content/admin/customer-token-authentication-token.htm
+
+* Sending logs from Windows Syslog agent - https://documentation.solarwinds.com/en/success_center/loggly/content/getting-started/gsg-send_data.htm
+
+* Snap agent - https://documentation.solarwinds.com/en/success_center/appoptics/content/kb/host_infrastructure/host_agent.htm
+
+* Installation - https://documentation.solarwinds.com/en/success_center/appoptics/content/kb/host_infrastructure/host_agent/installation_windows.htm
+
+* Organizing logs using tags - https://documentation.solarwinds.com/en/success_center/loggly/content/admin/tags.htm?cshid=loggly_tags
+
+* Source groups - https://documentation.solarwinds.com/en/success_center/loggly/content/admin/source-groups.htm?cshid=loggly_source-groups
+
+* Sending Data - https://documentation.solarwinds.com/en/success_center/loggly/content/admin/api-sending-data.htm?cshid=loggly_api-sending-data
